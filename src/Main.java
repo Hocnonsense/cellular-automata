6f61f873259ffda56bb3840f61fa1389c2d6d04b
Hocnonsense <48747984+Hocnonsense@users.noreply.github.com>
2019-04-25 21:45:30 +0800

    Update Main.java
    Yes. nothing new

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,10 @@
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 import MyUtil.OperateFrame;
 import autocell.*;
 import tmp.*;
 
 class Main {
 	public Main() {
-		new OperateFrame(new autoCell().controlGOC());
+		new OperateFrame(new AutoCell().controlGOL());
 	}
 	public static void main(String[] args) {
 		new Main();
